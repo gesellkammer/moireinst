7c24934acf3bc4033ca12e2cea89cbcd6008ece7
Eduardo Moguillansky <eduardo.moguillansky@gmail.com>
2014-02-28 22:32:50 +0100

    + new OSC backend: liblo + implemented serial reconnection upon connection dropped after sleep in OSX

--- midilamp/midilamp.py
+++ midilamp/midilamp.py
@@ -4,6 +4,7 @@ import serial
 import time
 import logging
 import sys
+import liblo
 
 SEARCH_TIMEOUT = 1 # listen to each connection for this ammount of time
 
@@ -43,7 +44,7 @@ def detect_arduino(timeout=10):
             try:
                 s = serial.Serial(portname, timeout=SEARCH_TIMEOUT)
                 t1 = time.time()
-                while time.time() - t1 < 3:
+                while time.time() - t1 < 1.5:
                     l = _readline(s)
                     if l and l == "HLMP":
                         device = portname
@@ -60,6 +61,9 @@ def detect_arduino(timeout=10):
     return device
 
 class MyOSCServer(OSC.OSCServer):
+    def __init__(self, *args, **kws):
+        OSC.OSCServer.__init__(self, *args, **kws)
+        self.timeout = 1
     def handle_timeout(self):
         self.timed_out = True
     def recv(self):
@@ -68,23 +72,21 @@ class MyOSCServer(OSC.OSCServer):
         return not self.timed_out
 
 class MidiLamp(object):
-    def __init__(self, oscport=11111, autostart=True):
+    def __init__(self, oscport=11111):
         self.conn = None
         self.connect_serial()
         self.oscport = oscport
         try:
-            self.server = MyOSCServer(("localhost", oscport))
+            # self.server = MyOSCServer(("localhost", oscport))
+            self.server = liblo.Server(11111)
         except:
-            print "Could not create OSC server. Is it already open?"
+            logging.debug("Could not create OSC server. Is it already open?")
             raise RuntimeError("osc error")
-        self.server.timeout = 1
         self._running = False
         self._lastlum = -1
-        self.add_handlers()
-        if autostart:
-            self.run()
-
-    def add_handlers(self):
+        self.add_handlers_liblo()
+        
+    def add_handlers_OSC(self):
         set_light = self.set_light
         def SET(path, tags, args, source):
             set_light(args[0])
@@ -92,6 +94,22 @@ class MidiLamp(object):
             self._running = False
         self.server.addMsgHandler("/set", SET)
         self.server.addMsgHandler("/stop", STOP)
+
+    def add_handlers_liblo(self):
+        set_light = self.set_light
+        def SET(path, args, types, src):
+            brightness = args[0]
+            if brightness > 1000:
+                logging.debug("Brightness should be 0-1000")
+            set_light(brightness)
+            # logging.debug("brightness: %d" % brightness)
+        def STOP(path, args, types, src):
+            self._running = False
+        def DEBUG(path, args, types, src):
+            logging.debug("Got garbage! path={path}, args={args}, types={types}".format(**locals()))
+        self.server.add_method("/set", None, SET)
+        self.server.add_method("/stop", None, STOP)
+        self.server.add_method(None, None, DEBUG)
         
     def set_light(self, lum, smooth=False):
         lum = min(999, lum)
@@ -106,35 +124,58 @@ class MidiLamp(object):
             connwrite(chr(0))
             self._lastlum = lum
         except serial.SerialTimeoutException:
-            print "could not write to serial, reconnecting"
+            logging.debug("could not write to serial, reconnecting")
             self.connect_serial(60)
 
-    def connect_serial(self, timeout=10):
+    def connect_serial(self, timeout=10, skip_detect=False):
         if self.conn:
+            logging.debug("trying to reconnect to %s. closing serial connection" % self.devname)
             self.conn.close()
-        self.devname = detect_arduino(timeout=timeout)
+        if not skip_detect:
+            self.devname = detect_arduino(timeout=timeout)
         if self.devname:
-            self.conn = serial.Serial(self.devname)
-            logging.debug("device found at port %s. Connected!" % self.devname) 
+            try:
+                self.conn = serial.Serial(self.devname, timeout=0.1)
+                logging.debug("device found at port %s. Connected!" % self.devname) 
+            except OSError:
+                logging.error("could not open device %s" % self.devname)
+                return False
         else:
             raise RuntimeError("device not found")
+        return True
     
     def close(self):
         self._running = False
         self.conn.close()
         time.sleep(0.1)
-        self.server.close()
-
+        
     def run(self):
         self._running = True
         oscserver = self.server
-        from time import sleep
+        from time import sleep, time
         logging.info("Listening to OSC on port: %s" % self.oscport)
+        last_incomming_serial = time()
         try:
             while self._running:
-                if not oscserver.recv(): # this will block until it receives a msg or it timesout
-                    # flush any incomming serial info each time we time out
-                    self.conn.flushInput()
+                now = time()
+                # this will block until it receives a msg or it timesout
+                if not oscserver.recv(50): 
+                    incomming = self.conn.read(1)
+                    if len(incomming):
+                        last_incomming_serial = now
+                        #self.conn.flushInput()
+                    else:
+                        if now - last_incomming_serial > 2:
+                            for N in range(5):
+                                logging.debug("lost connection! Will try to reconnect")
+                                ok = self.connect_serial(skip_detect=True)
+                                if ok: 
+                                    break
+                                else:
+                                    sleep(0.5)
+                            if not ok:
+                                self._running = False
+                                break
             self.close()
         except KeyboardInterrupt:
             print "exiting..."
@@ -142,7 +183,7 @@ class MidiLamp(object):
 
 
 if __name__ == '__main__':
-    l = MidiLamp(autostart=False)
+    l = MidiLamp()
     print "Press CTRL-C to exit"
     l.run()
 
